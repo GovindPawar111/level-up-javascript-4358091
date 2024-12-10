58be23ca199573da1693eca4dc2df698142dc2ed
Govind Pawar <pawargovind512@gmail.com>
2024-12-10 09:35:20 +0000

    completed first challange

--- start/01_01_available_books.js
+++ start/01_01_available_books.js
@@ -1,1 +1,26 @@
 // Write your code here
+class book {
+  constructor(title, author, isbn, numCopies) {
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
+    this.numCopies = numCopies;
+  }
+
+  getAvailability() {
+    if(this.numCopies <= 0){
+      return "out of stock";
+    }if(this.numCopies < 10){
+      return "in stock";
+    }
+    return "in stock";
+  }
+
+  sell(numSold = 1){
+    this.numCopies = this.numCopies - numSold;
+  }
+
+  restock(numCopies = 5){
+    this.numCopies = this.numCopies + numCopies;
+  }
+}
