909bd5e03529e2ac36bb68220de5557f96626418
Govind Pawar <pawargovind512@gmail.com>
2024-12-10 09:48:52 +0000

    completed second challange

--- start/01_02_movie_prototype.js
+++ start/01_02_movie_prototype.js
@@ -1,1 +1,35 @@
 // Write your code here
+
+class Movie {
+  constructor(title, director, genre, releaseYear, rating){
+    this.title = title;
+    this.director = director;
+    this.genre = genre;
+    this.releaseYear = releaseYear;
+    this.rating = rating;
+  }
+
+  getOverview(){
+    console.log(`${this.title}, a ${this.genre} film directed by ${this.director} was released in ${this.releaseYear}. It received a rating of ${this.rating}`);
+  }
+}
+
+const movie = new Movie('Animal', 'Vanga', 'Action', '2023', '4.5');
+
+movie.getOverview()
+
+function MovieFnPrototype(title, director, genre, releaseYear, rating){
+  this.title = title;
+  this.director = director;
+  this.genre = genre;
+  this.releaseYear = releaseYear;
+  this.rating = rating;
+}
+
+MovieFnPrototype.prototype.getOverview = function(){
+     console.log(`${this .title}, a ${this.genre} film directed by ${this.director} was released in ${this.releaseYear}. It received a rating of ${this.rating}`);
+}
+
+const movieFnPrototypeObj = new movieFnPrototype('Animal', 'Vanga', 'Action', '2023', '4.5');
+
+movieFnPrototypeObj.getOverview()
